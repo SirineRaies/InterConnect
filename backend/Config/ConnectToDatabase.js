723caf2094c70590816bd6f729331289d51ab136
sirine Raies <sirineraies20@gmail.com>
2026-01-06 02:39:13 +0100

    Déploiement Kubernetes sans secrets

--- backend/Config/ConnectToDatabase.js
+++ backend/Config/ConnectToDatabase.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_DATABASE_URI);
+    const mongoURI = process.env.MONGODB_DATABASE_URI;
+    
+    console.log(`Tentative de connexion à : ${mongoURI}`); 
+    
+    await mongoose.connect(mongoURI);
     console.log('Connexion à MongoDB réussie !');
   } catch (err) {
     console.error('Erreur de connexion à MongoDB :', err.message);
     process.exit(1);
   }
 };
+
 module.exports = connectToDatabase;
